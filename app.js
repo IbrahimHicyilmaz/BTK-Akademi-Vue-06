e3e4973d0badaec4243518465a856acc6d8ed165
IbrahimHicyilmaz <blog.ibrahimhicyilmaz5334@gmail.com>
2024-02-14 15:39:46 +0300

    06 commit

---
+++ app.js
@@ -0,0 +1,38 @@
+const app = Vue.createApp({
+    template:`
+    <h2>Frameworkler</h2>
+    <ul>
+        <li v-for="f in frameworks" class="list" :class="{selected:f.selected}" @click="selectTag(f)">
+        {{f.name}}
+        </li>
+    </ul>
+    <h3>Seçilen Frameworkler</h3>
+    <ul>
+        <li v-for="s in selectedTags">
+        {{s.name}}
+        </li>
+    </ul>
+    `,
+    data(){
+        return{
+            frameworks:[
+                {name:"Vue",selected:true},
+                {name:"React",selected:false},
+                {name:"Svelte",selected:false},
+                {name:"Angular",selected:false},
+            ]
+        }
+    },
+    methods: {
+        selectTag(f) {
+            f.selected=!f.selected;
+        }
+    },
+    computed:{
+        selectedTags() {
+            console.log("değişiklik oldu");
+            return selecteds = this.frameworks.filter(i=>i.selected)
+        }
+    }
+});
+app.mount('#app')
